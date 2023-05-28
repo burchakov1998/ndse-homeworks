a4145049c4968a30a660e72a4142d8aa1469ac91
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-05-28 22:37:35 +0300

    Update index.js

--- 002-console/index.js
+++ 002-console/index.js
@@ -1,1 +1,35 @@
+//Задание 1
 console.log(new Date());
+//Задание 2
+
+console.log('[ Игра угадай число ]')
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let genNum = Math.round(Math.random() * 100)
+
+const game = () => {
+readline.question('Введите число от 0 до 100: ', user_number => {
+    if(user_number<genNum){
+console.log(`user_number +'Больше'`);
+    
+    }
+    else if (user_number>genNum){
+console.log(`user_number + Меньше`);
+    }
+    else if (user_number==genNum){
+        Console.log(`Вы угадали ${genNum}`);
+    }
+    else{
+        console.log(`Ответ: "${genNum}"\n`);
+        readline.close();
+
+    }
+    readline.close();
+})}
+   
+
+game()
